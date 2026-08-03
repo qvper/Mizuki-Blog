2227c9f7c1ccfa59a5e41cd4c3c6c24015846e81
qvper <191046599+qvper@users.noreply.github.com>
2026-08-03 15:41:29 +0800

    修改了页面基础配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -5,7 +5,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
 	title: "Mizuki",
-	subtitle: "One demo website",
+	subtitle: "qvper's blog",
 	siteURL: "https://qvper.top/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-08-03", // 站点开始运行日期，用于站点统计组件计算运行天数
 
