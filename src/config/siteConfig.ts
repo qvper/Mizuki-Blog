dfa75e795983793ff0b36ab80ecbf9bde0f29e94
qvper <191046599+qvper@users.noreply.github.com>
2026-08-03 15:23:51 +0800

    更新了配置 | update configuration

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,13 +1,13 @@
 import type { SiteConfig } from "../types/config";
 
 // 定义站点语言
-const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
+const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
 	title: "Mizuki",
 	subtitle: "One demo website",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteURL: "https://qvper.top/", // 请替换为你的站点URL，以斜杠结尾
+	siteStartDate: "2026-08-03", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	lang: SITE_LANG,
 
