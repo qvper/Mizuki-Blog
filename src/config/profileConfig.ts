9c3cdd09f197e959cb52464595f484c98ce06dbc
qvper <191046599+qvper@users.noreply.github.com>
2026-08-06 22:43:35 +0800

    feat:更新用户简介和昵称

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -3,8 +3,8 @@ import type { ProfileConfig } from "../types/config";
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/xiyin.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "qvper",
-	bio: "世界は大きい、君は行かなければならない",
+	name: "qvper | 汐音",
+	bio: " 好きを追うだけで、満たされる",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
 		speed: 80, // 打字速度（毫秒）
