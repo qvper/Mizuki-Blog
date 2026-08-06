69d1ea06545013a9b1de0de87eae2f1b76734513
qvper <191046599+qvper@users.noreply.github.com>
2026-08-06 23:42:43 +0800

    feat:修改简介昵称格式

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -3,7 +3,7 @@ import type { ProfileConfig } from "../types/config";
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/xiyin.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "qvper | 汐音",
+	name: "qvper · 汐音",
 	bio: " 好きを追うだけで、満たされる",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
